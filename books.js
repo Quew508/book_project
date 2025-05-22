3933c7d2f814916c14a86a4b2f8345f262f2c5aa
Quew508 <quentin@propdata.net>
2025-05-22 21:23:46 +0200

    refactor to class

--- books.js
+++ books.js
@@ -7,30 +7,28 @@ const addBookForm = document.getElementById("addBookForm");
 let myLibrary = []
 let book;
 
-function Book(id, title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-  this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read ? 'read' : 'not read yet'}`
-  }
-}
+class Book {
+	constructor(id, title, author, pages, read) {
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
 
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
+	get info() {
+		return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'read' : 'not read yet'}`
+	}
+
+	toggleRead() {
+		this.read = !this.read;
+	}
 }
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(crypto.randomUUID(), title, author, pages, read);
   myLibrary.push(newBook);
 }
-
-const demoBook = new Book(1, "Demo Book", "Demo Author", 33, true);
 
 addBookToLibrary("The Odin Project", "Norsemen", 990, true);
 addBookToLibrary("The Odin Project, The Sequel", "Norsemen", 790, false);
